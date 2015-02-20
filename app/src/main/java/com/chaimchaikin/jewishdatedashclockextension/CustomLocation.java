585f255a9e4352312596baa04cfcdb30bdd878ee
chaimchaikin <github@chaimchaikin.za.net>
2015-02-19 21:49:43 -0500

    Initial commit

---
+++ app/src/main/java/com/chaimchaikin/jewishdatedashclockextension/CustomLocation.java
@@ -0,0 +1,129 @@
+package com.chaimchaikin.jewishdatedashclockextension;
+
+import android.content.Intent;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class CustomLocation extends FragmentActivity {
+
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private Location mLocation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_location);
+        setUpMapIfNeeded();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+    /**
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated.. This will ensure that we only ever
+     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * <p/>
+     * If it isn't installed {@link SupportMapFragment} (and
+     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+     * install/update the Google Play services APK on their device.
+     * <p/>
+     * A user can return to this FragmentActivity after following the prompt and correctly
+     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
+     * have been completely destroyed during this process (it is likely that it would only be
+     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
+     * method in {@link #onResume()} to guarantee that it will be called.
+     */
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p/>
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
+    private void setUpMap() {
+
+        double lat = 100;
+        double lng = 100;
+
+        Bundle b = this.getIntent().getExtras();
+        if(b!=null) {
+            lat = b.getDouble("lat");
+            lng = b.getDouble("lng");
+        }
+
+        mMap.setMyLocationEnabled(true);
+        mLocation = mMap.getMyLocation();
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lat, lng), 4.0f));
+        final Marker mMarker = mMap.addMarker(new MarkerOptions().position(new LatLng(lat, lng)).draggable(false));
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng coord) {
+                mMarker.setPosition(coord);
+
+
+                final Intent resultIntent = new Intent();
+                resultIntent.putExtra("lat", coord.latitude);
+                resultIntent.putExtra("lng", coord.longitude);
+
+                setResult(RESULT_OK, resultIntent);
+                finish();
+            }
+        });
+
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                marker.remove();
+                return false;
+            }
+        });
+
+        mMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+
+            @Override
+            public void onMarkerDrag(Marker marker) {
+
+            }
+            @Override
+            public void onMarkerDragEnd(Marker marker) {
+                LatLng newLocation = marker.getPosition();
+
+
+                //mLocation.setLatitude(newLocation.latitude);
+                //mLocation.setLongitude(newLocation.longitude);
+                //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(mLocation.getLatitude(), mLocation.getLongitude()), 15.0f));
+
+            }
+            @Override
+            public void onMarkerDragStart(Marker marker) {}
+
+        });
+    }
+}
---
+++ app/src/main/java/com/chaimchaikin/jewishdatedashclockextension/receiver/FireReceiver.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2013 two forty four a.m. LLC <http://www.twofortyfouram.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * <http://www.apache.org/licenses/LICENSE-2.0>
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
+package com.chaimchaikin.jewishdatedashclockextension.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.chaimchaikin.jewishdatedashclockextension.Constants;
+import com.chaimchaikin.jewishdatedashclockextension.JewishDateHelper;
+import com.chaimchaikin.jewishdatedashclockextension.LocationHelper;
+import com.chaimchaikin.jewishdatedashclockextension.TaskerPlugin;
+import com.chaimchaikin.jewishdatedashclockextension.bundle.BundleScrubber;
+import com.chaimchaikin.jewishdatedashclockextension.ui.EditActivity;
+
+import java.util.Locale;
+
+/**
+ * This is the "fire" BroadcastReceiver for a Locale Plug-in setting.
+ *
+ * @see com.twofortyfouram.locale.Intent#ACTION_FIRE_SETTING
+ * @see com.twofortyfouram.locale.Intent#EXTRA_BUNDLE
+ */
+public final class FireReceiver extends BroadcastReceiver
+{
+
+    /**
+     * @param context {@inheritDoc}.
+     * @param intent the incoming {@link com.twofortyfouram.locale.Intent#ACTION_FIRE_SETTING} Intent. This
+     *            should contain the {@link com.twofortyfouram.locale.Intent#EXTRA_BUNDLE} that was saved by
+     *            {@link EditActivity} and later broadcast by Locale.
+     */
+    @Override
+    public void onReceive(final Context context, final Intent intent)
+    {
+        /*
+         * Always be strict on input parameters! A malicious third-party app could send a malformed Intent.
+         */
+
+        if (!com.twofortyfouram.locale.Intent.ACTION_FIRE_SETTING.equals(intent.getAction()))
+        {
+            if (Constants.IS_LOGGABLE)
+            {
+                Log.e(Constants.LOG_TAG,
+                      String.format(Locale.US, "Received unexpected Intent action %s", intent.getAction())); //$NON-NLS-1$
+            }
+            return;
+        }
+
+        BundleScrubber.scrub(intent);
+
+        final Bundle bundle = intent.getBundleExtra(com.twofortyfouram.locale.Intent.EXTRA_BUNDLE);
+        BundleScrubber.scrub(bundle);
+
+  /*      if (PluginBundleManager.isBundleValid(bundle))
+        {
+
+            //final String message = bundle.getString(PluginBundleManager.BUNDLE_EXTRA_STRING_MESSAGE);
+            //Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+        }
+*/
+        if ( isOrderedBroadcast() ) {
+
+            setResultCode(TaskerPlugin.Setting.RESULT_CODE_OK);
+
+            JewishDateHelper jewishDate = new JewishDateHelper();
+
+
+            String locName;
+            double lat = 0;
+            double lng = 0;
+            String timezone;
+
+            if(bundle.getBoolean("loc_auto", false)) {
+                LocationHelper locHelper = new LocationHelper(context);
+                locHelper.updateLocation();
+
+                locName = locHelper.locationName;
+                lat = locHelper.lat;
+                lng = locHelper.lng;
+                timezone = locHelper.timezone;
+            } else {
+                locName = bundle.getString("loc_name");
+                lat = bundle.getDouble("loc_lat");
+                lng = bundle.getDouble("loc_lng");
+                timezone = bundle.getString("timezone");
+            }
+
+            jewishDate.setLocation(locName, lat, lng, timezone);
+
+            jewishDate.updateDates();
+
+            String hebrewDateTextShort = jewishDate.getShortDate();
+            String hebrewDateTextLong = jewishDate.getLongDate();
+            String descriptionString = jewishDate.getLongText();
+
+            if ( TaskerPlugin.Setting.hostSupportsVariableReturn( intent.getExtras() ) ) {
+
+                Bundle vars = new Bundle();
+                vars.putString( "%jd_short", hebrewDateTextShort );
+                vars.putString( "%jd_long", hebrewDateTextLong );
+                vars.putString( "%jd_desc", descriptionString );
+                vars.putString( "%jd_loc", locName );
+
+                TaskerPlugin.addVariableBundle( getResultExtras( true ), vars );
+            }
+        }
+    }
+}
